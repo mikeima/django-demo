5bccdc6006df8581c876028aac7b3f0eb5e921cd
mikeima <130894589+mikeima@users.noreply.github.com>
2023-06-20 23:27:05 +0300

    создание моделей (таблиц в БД) и добавление их в админку
    ./manage.py makemigrations
    ./manage.py migrate

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,144 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import (
+    Format, Material, Producer, K_B, Grammage, Ozm, IncomeDateTime,
+    T13_050, Invoice, Roll, ConsumptionDateTime, Consumption
+)
+
+
+@admin.register(Format)
+class FormatAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'format',
+                    'short_format',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'material_type',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'producer',
+                    )
+    list_per_page = 10
+
+
+@admin.register(K_B)
+class K_BAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'name',
+                    'short_name',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Grammage)
+class GrammageAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'grammage',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Ozm)
+class OzmAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'ozm',
+                    'k_b',
+                    'grammage',
+                    'producer',
+                    'material_type',
+                    )
+    list_per_page = 10
+
+
+@admin.register(IncomeDateTime)
+class IncomeDateTimeAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'income_datetime',
+                    'shift_number',
+                    )
+    list_per_page = 10
+
+
+@admin.register(T13_050)
+class T13_050Admin(admin.ModelAdmin):
+    list_display = ('code',
+                    'f13_050',
+                    'income_datetime',
+                    'pole1',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'code13_050',
+                    'pole1',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Roll)
+class RollAdmin(admin.ModelAdmin):
+    list_display = ('roll_number',
+                    'roll_weight',
+                    'k_b',
+                    'grammage',
+                    'roll_format',
+                    'producer',
+                    'material_type',
+                    'zone',
+                    'original_number',
+                    'defect',
+                    'vehicle',
+                    'invoice',
+                    'input_datetime',
+                    'cert_fsc',
+                    'upload_surname',
+                    )
+    list_per_page = 10
+
+
+@admin.register(ConsumptionDateTime)
+class ConsumptionDateTimeAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'consumption_datetime',
+                    'shift_number',
+                    'time',
+                    'material_type',
+                    'sum_weight',
+                    'defect',
+                    'pole1',
+                    )
+    list_per_page = 10
+
+
+@admin.register(Consumption)
+class ConsumptionAdmin(admin.ModelAdmin):
+    list_display = ('code',
+                    'roll_number',
+                    'code_datetime',
+                    'pole1',
+                    'pole2',
+                    'pole3',
+                    'pole4',
+                    'pole5',
+                    'material_type',
+                    'zone',
+                    'defect',
+                    'ozm',
+                    'cert_fsc',
+                    'pole6',
+                    'pole7',
+                    )
+    list_per_page = 10
--- app/models.py
+++ app/models.py
@@ -1,3 +1,289 @@
 from django.db import models
 
-# Create your models here.
+
+class Format(models.Model):
+    class Meta:
+        verbose_name = 'Формат'
+        verbose_name_plural = 'Форматы'
+
+    code = models.CharField(max_length=25,
+                            verbose_name='Код Формат')
+    format = models.CharField(max_length=25,
+                              verbose_name='Формат',
+                              null=True)
+    short_format = models.CharField(max_length=25,
+                                    verbose_name='Краткий формат',
+                                    null=True)
+
+    def __str__(self):
+        return f'{self.format}'
+
+
+class Material(models.Model):
+    class Meta:
+        verbose_name = 'Тип сырья'
+        verbose_name_plural = 'Типы сырья'
+
+    code = models.CharField(max_length=25,
+                            verbose_name='Код Тип сырья')
+    material_type = models.CharField(max_length=25,
+                                     verbose_name='Тип сырья',
+                                     null=True)
+
+    def __str__(self):
+        return f'{self.material_type}'
+
+
+class Producer(models.Model):
+    class Meta:
+        verbose_name = 'Поставщик'
+        verbose_name_plural = 'Поставщики'
+
+    code = models.CharField(max_length=25,
+                            verbose_name='Код Поставщик')
+    producer = models.CharField(max_length=25,
+                                verbose_name='Поставщик',
+                                null=True)
+
+    def __str__(self):
+        return f'{self.producer}'
+
+
+class K_B(models.Model):
+    class Meta:
+        verbose_name = 'К/Б'
+        verbose_name_plural = 'К/Б'
+
+    code = models.CharField(max_length=25,
+                            verbose_name='Код К/Б')
+    name = models.CharField(max_length=25,
+                            verbose_name='Название',
+                            null=True)
+    short_name = models.CharField(max_length=25,
+                                  verbose_name='Краткое обозначение',
+                                  null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Grammage(models.Model):
+    class Meta:
+        verbose_name = 'Граммаж'
+        verbose_name_plural = 'Граммаж'
+
+    code = models.CharField(max_length=25,
+                            verbose_name='Код Граммаж')
+    grammage = models.CharField(max_length=25,
+                                verbose_name='Граммаж',
+                                null=True)
+
+    def __str__(self):
+        return f'{self.grammage}'
+
+
+class Ozm(models.Model):
+    class Meta:
+        verbose_name = 'ОЗМ'
+        verbose_name_plural = 'ОЗМ'
+
+    code = models.IntegerField(verbose_name='Код ОЗМ')
+    ozm = models.CharField(max_length=25,
+                           verbose_name='ОЗМ',
+                           null=True)
+    k_b = models.CharField(max_length=255,
+                           verbose_name='Код К/Б',
+                           null=True)
+    grammage = models.CharField(max_length=25,
+                                verbose_name='Код Граммаж',
+                                null=True)
+    producer = models.CharField(max_length=25,
+                                verbose_name='Код Поставщик',
+                                null = True)
+    material_type = models.CharField(max_length=255,
+                                     verbose_name='Код Тип сырья',
+                                     null=True)
+
+    def __str__(self):
+        return f'{self.ozm}'
+
+
+class IncomeDateTime(models.Model):
+    class Meta:
+        verbose_name = 'Дата поступления'
+        verbose_name_plural = 'Даты поступления'
+
+    code = models.IntegerField(verbose_name='Код Дата поступления')
+    income_datetime = models.DateTimeField(verbose_name='Дата поступления',
+                                           null=True)
+    shift_number = models.CharField(max_length=25,
+                                    verbose_name='№ смены',
+                                    null=True)
+
+    def __str__(self):
+        return f'{self.income_datetime}'
+
+
+class T13_050(models.Model):
+    class Meta:
+        verbose_name = '13/050'
+        verbose_name_plural = '13/050'
+
+    code = models.IntegerField(verbose_name='Код 13/050')
+    f13_050 = models.CharField(max_length=25,
+                               verbose_name='13/050',
+                               null=True)
+    income_datetime = models.IntegerField(verbose_name='Код Дата поступления',
+                                          null=True)
+    pole1 = models.IntegerField(verbose_name='Поле 1',
+                                null=True)
+
+    def __str__(self):
+        return f'{self.f13_050}'
+
+
+class Invoice(models.Model):
+    class Meta:
+        verbose_name = 'Накладная'
+        verbose_name_plural = 'Накладные'
+
+    code = models.CharField(max_length=255,
+                            verbose_name='Код Накладная')
+    code13_050 = models.IntegerField(verbose_name='Код 13/050',
+                                     null=True)
+    pole1 = models.CharField(max_length=255,
+                             verbose_name='Поле1',
+                             null=True)
+
+    def __str__(self):
+        return f'{self.code}'
+
+
+class Roll(models.Model):
+    class Meta:
+        verbose_name = 'Рулон'
+        verbose_name_plural = 'Рулоны'
+
+    roll_number = models.CharField(max_length=25,
+                                   verbose_name='Код № Рулона')
+    roll_weight = models.IntegerField(verbose_name='Вес нетто/брутто',
+                                      null=True)
+    k_b = models.CharField(max_length=25,
+                           verbose_name='Код К/Б',
+                           null=True)
+    grammage = models.CharField(max_length=25,
+                                verbose_name='Код Граммаж',
+                                null=True)
+    roll_format = models.CharField(max_length=25,
+                                   verbose_name='Код Формат',
+                                   null=True)
+    producer = models.CharField(max_length=25,
+                                verbose_name='Код Поставщик',
+                                null=True)
+    material_type = models.CharField(max_length=25,
+                                     verbose_name='Код Тип сырья',
+                                     null=True)
+    zone = models.CharField(max_length=25,
+                            verbose_name='Зона',
+                            null=True)
+    original_number = models.CharField(max_length=255,
+                                       verbose_name='Исходный номер',
+                                       null=True)
+    defect = models.CharField(max_length=25,
+                              verbose_name='Брак',
+                              null=True)
+    vehicle = models.CharField(max_length=255,
+                               verbose_name='Тр_средство',
+                               null=True)
+    invoice = models.CharField(max_length=255,
+                               verbose_name='Код Накладная',
+                               null=True)
+    input_datetime = models.DateTimeField(verbose_name='Дата/время ввода',
+                                          null=True)
+    cert_fsc = models.CharField(max_length=255,
+                                verbose_name='Сертификат FSC',
+                                null=True)
+    upload_surname = models.CharField(max_length=255,
+                                      verbose_name='Выгрузка Фамилия',
+                                      null=True)
+    pole1 = models.IntegerField(verbose_name='Поле1',
+                                null=True)
+    pole2 = models.IntegerField(verbose_name='Поле2',
+                                null=True)
+
+    def __str__(self):
+        return f'{self.roll_number}'
+
+
+class ConsumptionDateTime(models.Model):
+    class Meta:
+        verbose_name = 'Дата расхода'
+        verbose_name_plural = 'Даты расхода'
+
+    code = models.IntegerField(verbose_name='Код Дата расхода')
+    consumption_datetime = models.DateTimeField(verbose_name='Дата расхода',
+                                                null=True)
+    shift_number = models.CharField(max_length=25,
+                                    verbose_name='№ смены',
+                                    null=True)
+    time = models.DateTimeField(verbose_name='Время',
+                                null=True)
+    material_type = models.CharField(max_length=255,
+                                     verbose_name='Тип сырья',
+                                     null=True)
+    sum_weight = models.IntegerField(verbose_name='Sum-Вес нетто/брутто',
+                                     null=True)
+    defect = models.CharField(max_length=255,
+                              verbose_name='Брак',
+                              null=True)
+    pole1 = models.IntegerField(verbose_name='Поле 1',
+                                null=True)
+
+    def __str__(self):
+        return f'{self.consumption_datetime}'
+
+
+class Consumption(models.Model):
+    class Meta:
+        verbose_name = 'Расход'
+        verbose_name_plural = 'Расход'
+
+    code = models.IntegerField(verbose_name='Код Расход')
+    roll_number = models.CharField(max_length=25,
+                                   verbose_name='Код № Рулона',
+                                   null=True)
+    code_datetime = models.IntegerField(verbose_name='Код Дата расхода')
+    pole1 = models.IntegerField(verbose_name='Поле1',
+                                null=True)
+    pole2 = models.IntegerField(verbose_name='Поле2',
+                                null=True)
+    pole3 = models.CharField(max_length=255,
+                             verbose_name='Поле3',
+                             null=True)
+    pole4 = models.CharField(max_length=255,
+                             verbose_name='Поле4',
+                             null=True)
+    pole5 = models.IntegerField(verbose_name='Поле5',
+                                null=True)
+    material_type = models.CharField(max_length=255,
+                                     verbose_name='Тип сырья',
+                                     null=True)
+    zone = models.CharField(max_length=255,
+                            verbose_name='Зона',
+                            null=True)
+    defect = models.CharField(max_length=255,
+                              verbose_name='Брак',
+                              null=True)
+    ozm = models.FloatField(verbose_name='ОЗМ',
+                            null=True)
+    cert_fsc = models.CharField(max_length=255,
+                                verbose_name='Сертификат FSC',
+                                null=True)
+    pole6 = models.IntegerField(verbose_name='Поле6',
+                                null=True)
+    pole7 = models.CharField(max_length=255,
+                             verbose_name='Поле7',
+                             null=True)
+
+    def __str__(self):
+        return f'{self.code}'
